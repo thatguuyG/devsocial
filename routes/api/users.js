2a2555a1677d9377cfc2d5f3d2ddbee4e3c47823
Glen <39155036+thatguuyG@users.noreply.github.com>
2020-12-02 22:22:40 +0300

    User registration added
    User registration to mongodb database added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route   GET api/users
 // @desc    Register users
@@ -9,12 +13,50 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
-], (req,res) => {
+], async(req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()});
     }
-    res.send('User route');
+
+    const {name, email, password} = req.body;
+
+    try{
+        //See if user exists
+        let user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
+        }
+
+        //Get user's gravatar(from the email)
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg', //pg rating to filter out adult content
+            d: 'mm'
+        });
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        //encrypt password using bcrypt
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+
+
+        res.send('User Registered');
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+    
+
+    //Return Jsonwebtoken
+
 });
 
 module.exports = router;
